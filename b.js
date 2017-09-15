3d21d754ee700ce8c96e6519a89935208c3973a5
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-15 18:17:30 +0700

    arr

---
+++ b.js
@@ -0,0 +1,13 @@
+const arrNum = [1, 3, 7, 8, 2];
+
+const arrPerson = [
+    { name: 'Teo', age: 10 },
+    { name: 'Ti', age: 12 },
+    { name: 'Tun', age: 14 }
+];
+
+//Person Company
+
+// console.log(arrNum.map(e => console.log(e)));
+console.log(arrPerson.map(person => person.age));
+// console.log(arrNum.map(person => person.name));
